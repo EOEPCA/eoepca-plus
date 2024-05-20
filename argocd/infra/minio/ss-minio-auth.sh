3d1acf7fecf1789f7ab165217973f5f9067b0db9
Richard Conway <richard@rconway.co.uk>
2024-05-20 10:41:14 +0100

    Update script for minio auth secret generation

--- argocd/infra/minio/ss-minio-auth.sh
+++ argocd/infra/minio/ss-minio-auth.sh
@@ -11,17 +11,20 @@ onExit() {
 trap onExit EXIT
 
 SECRET_NAME="minio-auth"
-NAMESPACE="infra"
 
 ROOT_USER="${1:-eoepca}"
 ROOT_PASSWORD="${2:-changeme}"
 
 secretYaml() {
-  kubectl -n "${NAMESPACE}" create secret generic "${SECRET_NAME}" \
+  kubectl create secret generic "${SECRET_NAME}" \
     --from-literal="rootUser=${ROOT_USER}" \
     --from-literal="rootPassword=${ROOT_PASSWORD}" \
     --dry-run=client -o yaml
 }
 
 # Create Secret and then pipe to kubeseal to create the SealedSecret
-secretYaml | kubeseal -o yaml --controller-name sealed-secrets --controller-namespace infra > templates/ss-${SECRET_NAME}.yaml
+# secretYaml | kubeseal -o yaml --controller-name sealed-secrets --controller-namespace infra > templates/ss-${SECRET_NAME}.yaml
+secretYaml \
+  | kubeseal -o yaml --controller-name sealed-secrets --controller-namespace infra \
+  | yq -r '"adminCredentials:\n  encryptedData:\n    rootPassword: \( .spec.encryptedData.rootPassword )\n    rootUser: \( .spec.encryptedData.rootUser )"' \
+  > admin-credentials.yaml

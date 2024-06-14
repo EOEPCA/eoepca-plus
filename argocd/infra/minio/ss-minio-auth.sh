fe9f779587b68cac0fdac57735bd49ad5604a9a6
Richard Conway <richard@rconway.co.uk>
2024-06-14 09:49:30 +0100

    Deploy minio

--- argocd/infra/minio/ss-minio-auth.sh
+++ argocd/infra/minio/ss-minio-auth.sh
@@ -28,6 +28,4 @@ secretYaml() {
 # Create Secret and then pipe to kubeseal to create the SealedSecret
 # secretYaml | kubeseal -o yaml --controller-name sealed-secrets --controller-namespace infra > templates/ss-${SECRET_NAME}.yaml
 secretYaml \
-  | kubeseal -o yaml --controller-name sealed-secrets --controller-namespace infra \
-  | yq -r '"adminCredentials:\n  encryptedData:\n    rootPassword: \( .spec.encryptedData.rootPassword )\n    rootUser: \( .spec.encryptedData.rootUser )"' \
-  > admin-credentials.yaml
+  | kubeseal -o yaml --controller-name sealed-secrets --controller-namespace infra > ss-${SECRET_NAME}.yaml

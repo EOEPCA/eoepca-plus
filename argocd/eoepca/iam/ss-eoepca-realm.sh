817b9584ce780ba7ad5a80efa4b0b1ae510259fe
w-scho <w-scho@werum.de>
2026-06-08 11:32:04 +0200

    IAM: Configure e-mail password config in initial realm config
    Also added (commented) example for identity provider config and removed obsolete sealed secret.

---
+++ argocd/eoepca/iam/ss-eoepca-realm.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+ORIG_DIR="$(pwd)"
+cd "$(dirname "$0")"
+BIN_DIR="$(pwd)"
+
+onExit() {
+  cd "${ORIG_DIR}"
+}
+trap onExit EXIT
+
+# Optional local .env file for secret values as env vars
+source .env 2>/dev/null
+
+SECRET_NAME="eoepca-realm"
+NAMESPACE="iam"
+
+SMTP_PASSWORD="${1:-${SMTP_PASSWORD:-changeme}}"
+#EOIAM_CLIENT_SECRET="${2:-${EOIAM_CLIENT_SECRET:-changeme}}"
+#GITHUB_CLIENT_SECRET="${3:-${GITHUB_CLIENT_SECRET:-changeme}}"
+
+secretYaml() {
+  kubectl -n "${NAMESPACE}" create secret generic "${SECRET_NAME}" \
+    --from-literal="smtp_password=${SMTP_PASSWORD}" \
+    --dry-run=client -o yaml
+}
+# TODO: Add when needed:
+#    --from-literal="eoiam_client_secret=${EOIAM_CLIENT_SECRET}" \
+#    --from-literal="github_client_secret=${GITHUB_CLIENT_SECRET}" \
+
+# Create Secret and then pipe to kubeseal to create the SealedSecret
+secretYaml \
+  | kubeseal -o yaml --controller-name sealed-secrets --controller-namespace infra > parts/ss-${SECRET_NAME}.yaml
